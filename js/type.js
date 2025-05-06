c7e8184e1aba26dc7bbc5f69764d09f5809288cb
jnneret <145510373+jnneret@users.noreply.github.com>
2025-05-06 08:00:30 -0700

    Update type.js

--- js/type.js
+++ js/type.js
@@ -1,29 +1,27 @@
 let string = `I SAID I LOVED YOU AND I WANTED`;
 let currentCharacter = 0;
 let typingSpeed = 0.1;
-let paperImg;
 let pageMargin = 25;
-
-function preload() {
-s; 
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  textFont('Georgia');
-  textSize(155);
+  textSize(165);
+  background('#F2E0BF'); 
 }
 
 function draw() {
-
-  imageMode(CORNER);
-  image(paperImg, 0, 0, width, height);
-
+  
   let currentString = string.substring(0, floor(currentCharacter));
 
   fill(0);
   textAlign(LEFT, TOP);
-  text(currentString, pageMargin + 40, pageMargin + 40, width - pageMargin * 2 - 40, height - pageMargin * 2 - 40);
+  text(
+    currentString,
+    pageMargin + 40,
+    pageMargin + 40,
+    width - pageMargin * 2 - 40,
+    height - pageMargin * 2 - 40
+  );
 
   currentCharacter += typingSpeed;
 }
